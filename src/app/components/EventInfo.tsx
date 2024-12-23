5d594ac5d0920f314cb689dc73b37557e2d748dc
fearnot221 <kenin221@gmail.com>
2024-12-23 15:46:33 +0800

    Remove notice

--- src/app/components/EventInfo.tsx
+++ src/app/components/EventInfo.tsx
@@ -9,8 +9,8 @@ const EventInfo: React.FC = () => {
 
   return (
     <section
-	  id='EventInfo'
-	  ref={infoRef}
+      id="EventInfo"
+      ref={infoRef}
       className={`max-w-5xl mx-auto bg-gray-900 bg-opacity-70 rounded-xl p-8 my-16 transition-opacity duration-1000 transform ${
         infoInView ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"
       }`}
@@ -48,13 +48,13 @@ const EventInfo: React.FC = () => {
         </div>
         <div>
           <h3 className="text-xl mb-4 text-indigo-400">活動須知</h3>
-          <div className="bg-gray-800 p-4 rounded">
+          {/* <div className="bg-gray-800 p-4 rounded">
             <h4 className="text-lg mt-4 mb-2 text-indigo-300">禁止事項</h4>
             <ul className="list-disc pl-5">
               <li>活動期間不可攜帶、飲用酒精飲料</li>
               <li>住宿期間不得有賭博、飲酒等違法行為</li>
             </ul>
-          </div>
+          </div> */}
         </div>
       </div>
     </section>
