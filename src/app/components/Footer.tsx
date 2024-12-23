98a14adb8e083fd7d97f9f81067a964537e34e02
fearnot221 <kenin221@gmail.com>
2024-12-23 15:45:16 +0800

    Update Footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,5 +1,20 @@
 import React from "react";
 import { useInView } from "react-intersection-observer";
+import { FaFacebookF, FaInstagram, FaYoutube, FaCode } from "react-icons/fa";
+import Link from "next/link";
+
+const CustomLink = ({ href, children, title }: any) => {
+  return (
+    <div className="relative group">
+      <Link href={href} target="_blank" className="hover:text-blue-600">
+        {children}
+      </Link>
+      <span className="absolute top-full left-1/2 transform -translate-x-1/2 mt-1 bg-black text-white p-1 rounded-lg text-sm max-w-[200px] w-[120px] opacity-0 group-hover:opacity-100 group-hover:translate-y-2 transition-all pointer-events-none flex items-center justify-center z-50">
+        {title}
+      </span>
+    </div>
+  );
+};
 
 const Footer: React.FC = () => {
   const { ref: footerRef, inView: footerInView } = useInView({
@@ -14,6 +29,58 @@ const Footer: React.FC = () => {
       }`}
     >
       <div className="container mx-auto px-4 text-center">
+        <div className="flex space-x-4 justify-center">
+          <CustomLink
+            title="Website"
+            href="https://scist.org"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-indigo-500 transition"
+          >
+            <FaCode size={20} />
+          </CustomLink>
+          <CustomLink
+            title="Facebook"
+            href="https://www.facebook.com/scist.tw/?locale=zh_TW"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-indigo-500 transition"
+          >
+            <FaFacebookF size={20} />
+          </CustomLink>
+          <CustomLink
+            title="Instagram"
+            href="https://www.instagram.com/scist.tw/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-indigo-500 transition"
+          >
+            <FaInstagram size={20} />
+          </CustomLink>
+          <CustomLink
+            title="Youtube"
+            href="https://www.youtube.com/c/OfficialSCIST"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-indigo-500 transition"
+          >
+            <FaYoutube size={20} />
+          </CustomLink>
+        </div>
+        <br />
+        <span style={{ display: "inline-flex" }}>
+          聯絡我們：
+          <CustomLink
+            href="mailto:courses@scist.org"
+            title="點擊發送郵件"
+            rel="noopener noreferrer"
+            className="hover:text-indigo-500 transition"
+          >
+            courses@scist.org
+          </CustomLink>
+        </span>
+        <br />
+        <br />
         <p>© 2025 SCIST 南臺灣學生資訊社群 | All Rights Reserved</p>
       </div>
     </footer>
--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,20 +1,5 @@
 import { FC } from "react";
-import { FaFacebookF, FaInstagram, FaYoutube, FaCode } from "react-icons/fa";
 import Countdown from "./Countdown";
-import Link from "next/link";
-
-const CustomLink = ({ href, children, title }: any) => {
-  return (
-    <div className="relative group">
-      <Link href={href} target="_blank" className="hover:text-blue-600">
-        {children}
-      </Link>
-      <span className="absolute top-full left-1/2 transform -translate-x-1/2 mt-1 bg-black text-white p-1 rounded-lg text-sm max-w-[200px] w-[120px] opacity-0 group-hover:opacity-100 group-hover:translate-y-2 transition-all pointer-events-none flex items-center justify-center">
-        {title}
-      </span>
-    </div>
-  );
-};
 
 const Test: FC = () => (
   <section className="min-h-screen flex flex-col items-center justify-center px-6 py-12 relative mt-10">
@@ -29,44 +14,6 @@ const Test: FC = () => (
         資深玩家
         <span aria-hidden="true">資深玩家</span>
       </p>
-    </div>
-    <div className="absolute top-6 right-6 flex space-x-4 text-indigo-300 -mt-12">
-      <CustomLink
-        title="官網"
-        href="https://scist.org"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="hover:text-indigo-500 transition"
-      >
-        <FaCode size={20} />
-      </CustomLink>
-      <CustomLink
-        title="Facebook"
-        href="https://www.facebook.com/scist.tw/?locale=zh_TW"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="hover:text-indigo-500 transition"
-      >
-        <FaFacebookF size={20} />
-      </CustomLink>
-      <CustomLink
-        title="Instagram"
-        href="https://www.instagram.com/scist.tw/"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="hover:text-indigo-500 transition"
-      >
-        <FaInstagram size={20} />
-      </CustomLink>
-      <CustomLink
-        title="Youtube"
-        href="https://www.youtube.com/c/OfficialSCIST"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="hover:text-indigo-500 transition"
-      >
-        <FaYoutube size={20} />
-      </CustomLink>
     </div>
     {/* <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold neon-text mt-10 tracking-widest animate-glitch text-center">
       2025 SCIST
